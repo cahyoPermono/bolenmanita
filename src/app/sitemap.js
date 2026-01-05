1fb5df75ab8ad227ca591c9277953d103cac219b
Cahyo Permono <cahyo.adi@imaniprima.com>
2026-01-05 20:51:26 +0700

    feat: Add sitemap and robots.txt for better SEO

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export const dynamic = 'force-static'
+
+export default function sitemap() {
+  return [
+    {
+      url: 'https://cahyoPermono.github.io/bolenmanita',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
